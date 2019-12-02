a94aad99c891e4aec39444e1edfeb91b26be6d3f
Gabriel Sanchez <gsanchez@Gabriels-MacBook-Pro.local>
2019-12-02 14:43:38 -0800

    m

--- Pods/Target Support Files/Pods-QueDijoTests/Pods-QueDijoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QueDijoTests : NSObject
-@end
-@implementation PodsDummy_Pods_QueDijoTests
-@end
--- Pods/Target Support Files/Pods-QueDijoUITests/Pods-QueDijoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QueDijoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_QueDijoUITests
-@end
